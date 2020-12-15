e50dae13bcc6bc38e62132229b2aa61c608058bd
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-15 23:49:09 +0100

    Update 1

---
+++ capitalize.js
@@ -0,0 +1,4 @@
+function capitalize(argument) {
+    let wordinput = argument;
+    return wordinput[0].toUpperCase() + wordinput.slice(1);
+}
---
+++ lastElement.js
@@ -0,0 +1,7 @@
+function lastElement(input) {
+    if (input.length === 0) {
+        return null;
+    } else {
+        return input[input.length - 1];
+    }
+}
